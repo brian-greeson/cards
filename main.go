206a36555d4388373780a5ed7581f12cd0a76223
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-08 23:38:53 -0600

    type cast and string join

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
 	cards.print()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	fmt.Println(cards.toString())
 
 }
