97c04a6376dd7e618cd0e39b9290e44e9782fc60
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-07-12 18:31:58 -0600

    Add file save and open test

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -9,4 +12,25 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 16 but got: %v", len(d))
 	}
 
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected First element to be 'Ace of Spades' got: %v", d[0])
+	}
+	if d[len(d)-1] != "Jack of Diamonds" {
+		t.Errorf("Expected Last element to be 'Jack of Diamonds' got: %v", d[len(d)-1])
+	}
+
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := deckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 go %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
